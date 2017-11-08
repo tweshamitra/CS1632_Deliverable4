dda1560269d5b08ecb1616cb3e1925bb3fa1cdf7
Twesha Mitra <twesha95@gmail.com>
2017-11-07 19:12:57 -0500

    mostly working

--- src/Element.java
+++ src/Element.java
@@ -12,7 +12,7 @@ public class Element{
         try{
             filename = args[0];
         } catch(Exception e){
-            System.out.println("Usage: <filename>");
+            System.out.println("Error: File not found");
             System.exit(1);
         }
         createHashmap(elements);  
@@ -27,52 +27,64 @@ public class Element{
             elements.put(parts[0], parts[1]);
         }
     }
-
     public static void readInputFile(String filename, HashMap elements) throws IOException{
-        Scanner s = new Scanner(new File(filename));
+        Scanner s = null;
+        try{
+            s = new Scanner(new File(filename));
+        }catch(FileNotFoundException e){
+            System.out.print("File " + filename + " not found");
+            System.exit(1);
+        }
         while(s.hasNext()){
             String name = s.nextLine();
-            System.out.println(name);
             ArrayList<String> symbols = new ArrayList<String>();
             String symbol= "";
             boolean clear = false;
-            for (int i = 0; i < name.length(); i++){
                 try{
-                    if(elements.containsKey(name.charAt(i))){
-                        symbol = "" + name.charAt(i);
-                        symbols.add(symbol);
-                    }
-                    else if(elements.containsKey(name.substring(i,i+2))){
-                        symbol = name.substring(i, i+2);
-                        symbols.add(symbol);
-                        i++;                        
-                        }    
-                    else if(name.charAt(i) == ' '){
-                        continue;
-                    }
-                    else {
-                        System.out.println("here");
-                        clear = true;
-                        symbols.clear();
-                        break;
-                    }                
-                    
+                    for (int i = 0; i < name.length(); i++){
+                        if(elements.containsKey(name.charAt(i))){
+                            symbol = "" + name.charAt(i);
+                            symbols.add(symbol);
+                        }
+                        else if(elements.containsKey(name.substring(i,i+2))){
+                            symbol = name.substring(i, i+2);
+                            symbols.add(symbol);
+                            i++;                        
+                            }    
+                        else if(name.charAt(i) == ' '){
+                            continue;
+                        }
+                        else {
+                            System.out.println("Could not create name, " + name + ", out of elements" );
+                            clear = true;
+                            break;
+                        }                
+                    }  
                     for(String sym : symbols){
                         if(clear == true){
                             symbols.clear();
+                            break;
                         }
                         else{
-                            System.out.print(sym + " ");
+                            System.out.print(sym + " - ");
                         }
                     }
-                    
+                    System.out.println("\n");
+
+                    for(String sym : symbols){
+                        if(clear == true){
+                            symbols.clear();
+                            break;
+                        }
+                        else{
+                            System.out.print(elements.get(sym) + " - ");
+                        }
+                    }
+                    System.out.println("\n");
                     symbols.clear();
                 } catch(Exception e){
 
                 }
-                
-            }
-            System.out.println(" ");
        }
     }
   
